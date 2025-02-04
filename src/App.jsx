dd86cec3349cbe39fd60672152d7ceaf588b052a
Anurag <anuragkamboj20@gmail.com>
2025-02-04 16:48:54 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from '/src/Components/Navbar';
 import './App.css';
 import Homepage from './pages/Homepage';
 
+// some changes.
 const App = () => {
   return (
     <div className="App">
