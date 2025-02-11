aa7cf81a7e05fcdc48179780aac37d48a3a8b57d
anjali ahlawat <anjaliahlawat45@gmail.com>
2025-02-11 17:16:58 +0530

    inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,28 @@ import React from 'react';
 import Navbar from '/src/Components/Navbar';
 import './App.css';
 import Homepage from './pages/Homepage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Registration from './componets/registrationPopUp.jsx';
+import Nonloginpage from './pages/nonLoginPage.jsx';
+import DashboardHomePage from './pages/dashboardHomePage.jsx';
+import DashboardPastEvents from './pages/dashboardPastEvents.jsx';
+import DashboardBookmarkEvents from './pages/dashboardBookmark.jsx';
 
-
-// feature/detailed-page branch
-//changes in master
-//testing
-const App = () => {
-  return (
-    <div className="App">
-      <Homepage/>
-    </div>
-  );
-};
-
-export default App;
+const App=()=>
+    {
+     return(
+        <BrowserRouter>
+        <Routes>
+         <Route path="/" element={<Homepage/>}></Route>
+          <Route path="/nonlogin" element={<Nonloginpage/>}></Route>
+          <Route path="/register" element={<Registration />}></Route>
+           <Route path="/dashboard" element={<DashboardHomePage />}></Route> 
+            <Route path="/dashboard/past" element={<DashboardPastEvents />}></Route>
+            <Route path="/dashboard/bookmark" element={<DashboardBookmarkEvents />}></Route>
+       
+         
+        </Routes>
+        </BrowserRouter>
+     );
+    }
+    export default App;
